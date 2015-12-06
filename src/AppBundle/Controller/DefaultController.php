e288300d8fab9bbb2a3cf65a79558452a651e8fa
bhd.m@ya.ru <bhd.m@ya.ru>
2015-12-07 02:44:14 +0300

    u

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Country;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -43,4 +45,69 @@ class DefaultController extends Controller
         return ['isPost' => $isPost];
     }
 
+
+    /**
+     * @Route("/country/info/set", name="country_info_set")
+     */
+    public function setCountryAction(){
+        $em = $this->getDoctrine()->getManager();
+        $title = 'Бельгия';
+        $body = ' <div class="col-xs-5"><img src="/bundles/app/images/img-country-3.png"></div>
+                    <div class="col-xs-7">
+                        <table class="tbl">
+                            <tr>
+                                <td class="th1" style="width: 120px">Тип визы</td>
+                                <td class="th2" style="width: 150px">Срок оформления</td>
+                                <td class="th3">Подача документов через аккредитованное агентство</td>
+                                <td class="th4">Личная подача документов в визовый центр</td>
+                            </tr>
+                            <tr>
+                                <td>до 15 дней</td>
+                                <td>7-10 рабочих дней</td>
+                                <td></td>
+                                <td class="euro">30€</td>
+                            </tr>
+                            <tr>
+                                <td>до 30/90 дней</td>
+                                <td>7-10 рабочих дней</td>
+                                <td class="euro">270€</td>
+                                <td class="euro">200€</td>
+                            </tr>
+                            <tr>
+                                <td>90/180 бизн.млт.</td>
+                                <td>7-10 рабочих дней</td>
+                                <td class="euro">400€</td>
+                                <td class="euro">500€</td>
+                            </tr>
+                            <tr>
+                                <td>180/365 бизн.млт.</td>
+                                <td>7-10 рабочих дней</td>
+                                <td class="euro">500€</td>
+                                <td class="euro">350€</td>
+                            </tr>
+                            <tr>
+                                <td>3 года бизн.млт.</td>
+                                <td>7-10 рабочих дней</td>
+                                <td class="euro">600€</td>
+                                <td class="euro">450€</td>
+                            </tr>
+                        </table>
+                    </div>';
+
+        $country = new Country();
+        $country->setTitle($title);
+        $country->setBody($body);
+        $em->persist($country);
+        $em->flush($country);
+
+        return new Response($body);
+    }
+
+    /**
+     * @Route("/country/info", name="country_info")
+     */
+    public function countryAction(Request $request){
+        $country = $this->getDoctrine()->getRepository('AppBundle:Country')->find($request->request->get('id'));
+        return new Response($country->getBody());
+    }
 }
